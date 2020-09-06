b9cf4966a3e3d0d1f86d54799ac6213ebcb03edc
Nordic Energy <54575925+nordicenergy@users.noreply.github.com>
2020-09-06 11:16:08 +0300

    Update ERC20Burnable.sol

--- contracts/open-zeppelin-contracts/token/ERC20/ERC20Burnable.sol
+++ contracts/open-zeppelin-contracts/token/ERC20/ERC20Burnable.sol
@@ -1,26 +1,40 @@
-pragma solidity ^0.4.24;
+// SPDX-License-Identifier: MIT
 
+pragma solidity ^0.6.0;
+
+import "../../GSN/Context.sol";
 import "./ERC20.sol";
 
 /**
- * @title Burnable Token
- * @dev Token that can be irreversibly burned (destroyed).
+ * @dev Extension of {ERC20} that allows token holders to destroy both their own
+ * tokens and those that they have an allowance for, in a way that can be
+ * recognized off-chain (via event analysis).
  */
-contract ERC20Burnable is ERC20 {
+abstract contract ERC20Burnable is Context, ERC20 {
     /**
-     * @dev Burns a specific amount of tokens.
-     * @param value The amount of token to be burned.
+     * @dev Destroys `amount` tokens from the caller.
+     *
+     * See {ERC20-_burn}.
      */
-    function burn(uint256 value) public {
-        _burn(msg.sender, value);
+    function burn(uint256 amount) public virtual {
+        _burn(_msgSender(), amount);
     }
 
     /**
-     * @dev Burns a specific amount of tokens from the target address and decrements allowance
-     * @param from address The address which you want to send tokens from
-     * @param value uint256 The amount of token to be burned
+     * @dev Destroys `amount` tokens from `account`, deducting from the caller's
+     * allowance.
+     *
+     * See {ERC20-_burn} and {ERC20-allowance}.
+     *
+     * Requirements:
+     *
+     * - the caller must have allowance for ``accounts``'s tokens of at least
+     * `amount`.
      */
-    function burnFrom(address from, uint256 value) public {
-        _burnFrom(from, value);
+    function burnFrom(address account, uint256 amount) public virtual {
+        uint256 decreasedAllowance = allowance(account, _msgSender()).sub(amount, "ERC20: burn amount exceeds allowance");
+
+        _approve(account, _msgSender(), decreasedAllowance);
+        _burn(account, amount);
     }
 }
