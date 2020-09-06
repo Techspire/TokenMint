35148335f2f49ac5e4398be03528ad3b1132075b
Nordic Energy <54575925+nordicenergy@users.noreply.github.com>
2020-09-06 11:17:01 +0300

    Update ERC20Capped.sol

--- contracts/open-zeppelin-contracts/token/ERC20/ERC20Capped.sol
+++ contracts/open-zeppelin-contracts/token/ERC20/ERC20Capped.sol
@@ -1,28 +1,41 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.6.0;
 
-import "./ERC20Mintable.sol";
+import "./ERC20.sol";
 
 /**
- * @title Capped token
- * @dev Mintable token with a token cap.
+ * @dev Extension of {ERC20} that adds a cap to the supply of tokens.
  */
-contract ERC20Capped is ERC20Mintable {
+abstract contract ERC20Capped is ERC20 {
     uint256 private _cap;
 
+    /**
+     * @dev Sets the value of the `cap`. This value is immutable, it can only be
+     * set once during construction.
+     */
     constructor (uint256 cap) public {
-        require(cap > 0);
+        require(cap > 0, "ERC20Capped: cap is 0");
         _cap = cap;
     }
 
     /**
-     * @return the cap for the token minting.
+     * @dev Returns the cap on the token's total supply.
      */
     function cap() public view returns (uint256) {
         return _cap;
     }
 
-    function _mint(address account, uint256 value) internal {
-        require(totalSupply().add(value) <= _cap);
-        super._mint(account, value);
+    /**
+     * @dev See {ERC20-_beforeTokenTransfer}.
+     *
+     * Requirements:
+     *
+     * - minted tokens must not cause the total supply to go over the cap.
+     */
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override {
+        super._beforeTokenTransfer(from, to, amount);
+
+        if (from == address(0)) { // When minting tokens
+            require(totalSupply().add(amount) <= _cap, "ERC20Capped: cap exceeded");
+        }
     }
 }
