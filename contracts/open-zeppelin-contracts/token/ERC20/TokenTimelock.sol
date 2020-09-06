34271e54fb5522ba2772722c43cec6f0f75e4d54
Nordic Energy <54575925+nordicenergy@users.noreply.github.com>
2020-09-06 11:17:41 +0300

    Update TokenTimelock.sol

--- contracts/open-zeppelin-contracts/token/ERC20/TokenTimelock.sol
+++ contracts/open-zeppelin-contracts/token/ERC20/TokenTimelock.sol
@@ -1,11 +1,13 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.6.0;
 
 import "./SafeERC20.sol";
 
 /**
- * @title TokenTimelock
- * @dev TokenTimelock is a token holder contract that will allow a
- * beneficiary to extract the tokens after a given release time
+ * @dev A token holder contract that will allow a beneficiary to extract the
+ * tokens after a given release time.
+ *
+ * Useful for simple vesting schedules like "advisors get all of their tokens
+ * after 1 year".
  */
 contract TokenTimelock {
     using SafeERC20 for IERC20;
@@ -20,8 +22,8 @@ contract TokenTimelock {
     uint256 private _releaseTime;
 
     constructor (IERC20 token, address beneficiary, uint256 releaseTime) public {
-        // solium-disable-next-line security/no-block-members
-        require(releaseTime > block.timestamp);
+        // solhint-disable-next-line not-rely-on-time
+        require(releaseTime > block.timestamp, "TokenTimelock: release time is before current time");
         _token = token;
         _beneficiary = beneficiary;
         _releaseTime = releaseTime;
@@ -51,13 +53,14 @@ contract TokenTimelock {
     /**
      * @notice Transfers tokens held by timelock to beneficiary.
      */
-    function release() public {
-        // solium-disable-next-line security/no-block-members
-        require(block.timestamp >= _releaseTime);
+    function release() public virtual {
+        // solhint-disable-next-line not-rely-on-time
+        require(block.timestamp >= _releaseTime, "TokenTimelock: current time is before release time");
 
         uint256 amount = _token.balanceOf(address(this));
-        require(amount > 0);
+        require(amount > 0, "TokenTimelock: no tokens to release");
 
         _token.safeTransfer(_beneficiary, amount);
     }
 }
+
